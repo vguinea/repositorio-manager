f4d4047a101629b6f65406423d3ea35dbd394a15
vguinea <48250364+vguinea@users.noreply.github.com>
2025-12-03 19:57:37 +0100

    Create Figura.java

---
+++ Figura.java
@@ -0,0 +1,9 @@
+package es.gva.edu.sanchezgarcia.jg.mavenproject54;
+/**
+* Interfaz Figura: Es una generalización de una figura geométrica.
+* @author José Gaspar Sánchez García.
+*/
+public interface Figura {
+ public double calcularArea();
+ public double calcularPerimetro();
+}
